1a5e660bad5668728e64914879253fc8e5552221
Kai Page <kai@quantaly.net>
2024-01-12 15:08:18 -0700

    Add deployment script

--- build.sh
+++ build.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
 GOBUILDFLAGS="-v -trimpath -ldflags -s"
+TARGET_DIR="out/$(go env GOOS)/$(go env GOARCH)"
 
-mkdir -p out
-go build $GOBUILDFLAGS -o out/lets-hit-the-gym .
+mkdir -p $TARGET_DIR
+go build $GOBUILDFLAGS -o $TARGET_DIR/lets-hit-the-gym .
